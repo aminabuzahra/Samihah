ac538264a505f7473b1256dbda79a09e04d9fec0
Amin Abu-Zahra <purenewart@gmail.com>
2024-10-26 19:56:07 +0300

    Very important update to my code !!!

--- Assets/Scripts/GitHubTest.cs
+++ Assets/Scripts/GitHubTest.cs
@@ -7,7 +7,7 @@ public class GitHubTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log ("Hi Samihah");
     }
 
     // Update is called once per frame
