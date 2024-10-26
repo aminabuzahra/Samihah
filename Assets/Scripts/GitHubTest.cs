c40c3b8a85f1b274e3db8b0e65ccbf8247293fbc
Amin Abu-Zahra <purenewart@gmail.com>
2024-10-26 19:58:11 +0300

    Another So Important Update !!

--- Assets/Scripts/GitHubTest.cs
+++ Assets/Scripts/GitHubTest.cs
@@ -8,6 +8,7 @@ public class GitHubTest : MonoBehaviour
     void Start()
     {
         Debug.Log ("Hi Samihah");
+        Debug.Log ("Hi Fathieh");
     }
 
     // Update is called once per frame
