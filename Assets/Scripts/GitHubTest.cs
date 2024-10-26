ac8763e85ea37c475c8aea15edb1fb886b72cc0d
Amin Abu-Zahra <purenewart@gmail.com>
2024-10-26 21:36:26 +0300

    adding more

--- Assets/Scripts/GitHubTest.cs
+++ Assets/Scripts/GitHubTest.cs
@@ -1,20 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
 
 public class GitHubTest : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
-    {
-        Debug.Log ("Hi Samihah");
-        Debug.Log ("Hi Fathieh");
-        Debug.Log("Hi Subhieh");
-    }
+    { 
+        //    string firstName = "Khamis";
+        //    string lastName = "Sabri";
 
-    // Update is called once per frame
-    void Update()
-    {
+        //    Debug.Log (firstName.Length);
+
+        //    Debug.Log (firstName + " " +  lastName);
+        //    Debug.Log ( 1 + "Saleem"); 
+
+        // Variable Scopes
+      
+
+        // {
+        //     int a = 5;
+        //     Debug.Log(a);
+        //     {
+        //        Debug.Log(a);
+        //     }
+        // }
         
-    }
+        // int counter = 0;
+        // while (counter < 10)
+        // {
+        //    int randomNumber = Random.Range(1,7); // 1, 2, 3, 4, 5, 6
+        //    Debug.Log(randomNumber); 
+        //    counter++;
+        //    if (randomNumber == 4) 
+        //    {
+        //     // break;
+        //      continue;
+        //    }
+        //    Debug.Log("Subhi");
+        //    Debug.Log("Mlukhieh");
+        // }
+        // int tuna = 10;
+        // int tomato = 5;
+        // int sardines = 10;
+        // int foul = 20;
+
+        // int[] shop = new int[4];
+
+        // int[] shop = new int[4] {10, 5, 10, 20};
+
+        int[] shop = {10, 5, 10, 20};
+
+        // Debug.Log (shop.Length);
+
+        int counter = 0;
+        while (counter < shop.Length)
+        {
+            Debug.Log(shop[counter]);
+            counter ++;
+        }
+
+}
 }
